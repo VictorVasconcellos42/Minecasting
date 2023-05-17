c296f81c1ae22b31e397a62914d05ef11819ebd4
vde-vasc <vde-vasc@student.42.rio>
2023-05-17 14:57:08 -0300

    include: cub3d header created

---
+++ include/cub3d.h
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cub3d.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: thfirmin <thfirmin@student.42.rio>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/07 01:59:28 by thfirmin          #+#    #+#             */
+/*   Updated: 2023/05/16 17:12:25 by thfirmin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CUB3D_H
+# define CUB3D_H
+
+# include <stdio.h>
+# include <errno.h>
+# include <string.h>
+# include <fcntl.h>
+# include <math.h>
+# include "libft.h"
+# include "mlx.h"
+# include "get_next_line.h"
+
+typedef struct s_cube
+{
+	char	**map;
+	char	**path;
+}	t_cube;
+
+#endif
