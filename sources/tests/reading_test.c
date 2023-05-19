2e4bcc6497922c7a16131733152b07b9290aaa97
vitorsantanna2 <95882160+vitorsantanna2@users.noreply.github.com>
2023-05-19 16:27:42 -0300

    Fixed Norminette

--- sources/tests/reading_test.c
+++ sources/tests/reading_test.c
@@ -1,12 +1,22 @@
-
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   reading_test.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jsantann <jsantann@student.42.rio>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/19 16:25:01 by jsantann          #+#    #+#             */
+/*   Updated: 2023/05/19 16:25:54 by jsantann         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 #include "parse.h"
 
-int main(int argc, char **argv)
+int	main(int argc, char **argv)
 
 {
-	t_cube cube;
-	
-	int fd;
+	t_cube	cube;
+	int		fd;
+
 	fd = error_menu(argv, argc);
 	get_file(fd, &cube);
 }
