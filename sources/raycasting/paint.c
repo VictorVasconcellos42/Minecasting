335a79dc7822e43cae887ea2a3dedfb8f76e7a41
vde-vasc <vde-vasc@student.42.rio>
2023-05-24 07:43:21 -0300

    Feature: draw_cube function create

--- sources/raycasting/paint.c
+++ sources/raycasting/paint.c
@@ -6,7 +6,7 @@
 /*   By: vde-vasc <vde-vasc@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/23 07:26:23 by vde-vasc          #+#    #+#             */
-/*   Updated: 2023/05/23 07:39:22 by vde-vasc         ###   ########.fr       */
+/*   Updated: 2023/05/23 10:13:55 by vde-vasc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,4 +38,19 @@ void	draw_vline(t_cube *cube, int x, int y1, int y2, int color)
 	i = y1;
 	while (i < y2)
 		put_pixel(cube, x, i++, color);
+}
+
+void	draw_cube(t_cube *cube, int x, int y, int color)
+{
+	int i;
+	int j;
+
+	i = 0;
+	while (i < y)
+	{
+		j = 0;
+		while (j < x)
+			put_pixel(cube, j++, i, color);
+		i++;
+	}
 }
