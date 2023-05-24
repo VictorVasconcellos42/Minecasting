a7a4c14e816bd6e72b31129e548adb37419c3f38
vde-vasc <vde-vasc@student.42.rio>
2023-05-24 07:46:08 -0300

    System: add prototype in header

--- include/cub3d.h
+++ include/cub3d.h
@@ -6,7 +6,7 @@
 /*   By: vde-vasc <vde-vasc@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/07 01:59:28 by thfirmin          #+#    #+#             */
-/*   Updated: 2023/05/23 07:44:55 by vde-vasc         ###   ########.fr       */
+/*   Updated: 2023/05/23 10:10:50 by vde-vasc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,6 +38,7 @@ typedef struct s_cube
 
 void	draw_line(t_cube *cube, int x1, int x2, int y, int color);
 void	draw_vline(t_cube *cube, int x, int y1, int y2, int color);
+void	draw_cube(t_cube *cube, int x, int y, int color);
 void	put_pixel(t_cube *cube, int x, int y, int color);
 
 #endif
