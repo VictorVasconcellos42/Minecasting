f64bae3901a8341006f267580d5371f7e0101fc5
vitorsantanna2 <95882160+vitorsantanna2@users.noreply.github.com>
2023-05-25 13:42:21 -0300

    style: renamemap_reading to scene_reading

---
+++ sources/model/scene_reading/matrix_utils.c
@@ -0,0 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   matrix_utils.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vde-vasc <vde-vasc@student.42.rio>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/16 23:33:26 by jsantann          #+#    #+#             */
+/*   Updated: 2023/05/24 11:16:18 by vde-vasc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "parse.h"
+
+int	count_matrix(char **matrix)
+{
+	int	i;
+
+	i = 0;
+	while (matrix[i])
+		i++;
+	return (i);
+}
+
+void	free_matrix(char **matrix)
+{
+	int	i;
+
+	i = 0;
+	while (matrix[i])
+	{
+		free(matrix[i]);
+		i++;
+	}
+	free(matrix);
+}
+
+void	print_matrix(t_cube *cub)
+{
+	int	i;
+
+	i = 0;
+	printf("%s\n", cub->d_map.resolution);
+	while (cub->d_map.texture[i])
+		printf("%s\n", cub->d_map.texture[i++]);
+	i = 0;
+	printf("%s\n", cub->d_map.sprites);
+	i = 0;
+	while (cub->d_map.colors[i])
+		printf("%s\n", cub->d_map.colors[i++]);
+	i = 0;
+	while (cub->d_map.map[i])
+		printf("%s", cub->d_map.map[i++]);
+}
